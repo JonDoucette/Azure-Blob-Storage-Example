b33fa0991ebc6fab5214d4156b811e6eb8e9e396
Jon <jonrdoucette@gmail.com>
2023-08-30 14:47:17 -0700

    Add project files.

---
+++ Azure Blob Storage/FilesClient.cs
@@ -0,0 +1,109 @@
+﻿using Azure.Storage;
+using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure_Blob_Storage
+{
+    public class FilesClient
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly BlobContainerClient _defaultContainerClient;
+
+        public FilesClient(string defaultContainer)
+        {
+            var endpoint = "https://lms365jondevstorage.blob.core.windows.net/";
+
+            var account = "lms365jondevstorage";
+
+            var key = Environment.GetEnvironmentVariable("BlobStorageKey", EnvironmentVariableTarget.User);
+
+            var storageEndpoint = new Uri(endpoint);
+            var storageCredentials = new StorageSharedKeyCredential(account, key);
+            _blobServiceClient = new BlobServiceClient(storageEndpoint, storageCredentials);
+            _defaultContainerClient = CreateContainerIfNotExists(defaultContainer).Result;
+        }
+
+        private async Task<BlobContainerClient> CreateContainerIfNotExists(string container)
+        {
+            var lowerCaseContainer = container.ToLower();
+            var containerClient = _blobServiceClient.GetBlobContainerClient(lowerCaseContainer);
+            if (!await containerClient.ExistsAsync())
+            {
+                containerClient = await _blobServiceClient.CreateBlobContainerAsync(lowerCaseContainer);
+            }
+
+            return containerClient;
+        }
+
+        public Task UploadFile(string file)
+        {
+            return UploadFileInternal(file, _defaultContainerClient);
+        }
+
+        public async Task UploadFile(string file, string container)
+        {
+            var containerClient = await CreateContainerIfNotExists(container);
+            await UploadFileInternal(file, containerClient);
+        }
+        private Task UploadFileInternal(string file, BlobContainerClient client)
+        {
+            var data = new BinaryData(File.ReadAllBytes(file));
+            return client.UploadBlobAsync(Path.GetFileName(file), data);
+        }
+
+        public async Task DownloadFile(string filename, string container, string downloadDirectory)
+        {
+            var containerClient = await CreateContainerIfNotExists(container);
+            await DownloadFileInternal(filename, downloadDirectory, containerClient);
+        }
+
+        public Task DownloadFile(string filename, string downloadDirectory)
+        {
+            return DownloadFileInternal(filename, downloadDirectory, _defaultContainerClient);
+        }
+
+        private Task DownloadFileInternal(string filename, string downloadDirectory, BlobContainerClient client)
+        {
+            var blobClient = client.GetBlobClient(filename);
+            var downloadedFile = Path.Combine(downloadDirectory, filename);
+
+            if (!Directory.Exists(downloadDirectory))
+            {
+                Directory.CreateDirectory(downloadDirectory);
+            }
+
+            if (!File.Exists(downloadedFile))
+            {
+                var stream = File.Create(downloadedFile);
+                stream.Dispose();
+            }
+
+            return blobClient.DownloadToAsync(downloadedFile);
+        }
+        public async Task DeleteFile(string filename, string container)
+        {
+            var containerClient = await CreateContainerIfNotExists(container);
+            await DeleteFileInternal(filename, containerClient);
+        }
+
+        public Task DeleteFile(string filename)
+        {
+            return DeleteFileInternal(filename, _defaultContainerClient);
+        }
+
+        private async Task DeleteFileInternal(string filename, BlobContainerClient blobContainer)
+        {
+            var file = blobContainer.GetBlobClient(filename);
+            var fileExists = await file.ExistsAsync();
+            if (fileExists)
+            {
+                await blobContainer.DeleteBlobAsync(filename);
+            }
+        }
+
+    }
+}
---
+++ Azure Blob Storage/Program.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Storage.Blobs.Models;
+
+namespace Azure_Blob_Storage
+{
+    internal class Program
+    {
+        public const string Downloads = "Downloads";
+        public const string Uploads = "Uploads";
+        static async Task Main(string[] args)
+        {
+            var client = new FilesClient("Activity04");
+            var filename1 = "Test1.txt";
+            await client.DeleteFile(filename1);
+            var fullpath1 = Path.Combine(Uploads, filename1);
+            Console.Write(File.Exists(fullpath1));
+            await client.UploadFile(fullpath1);
+            await client.DownloadFile(filename1, Downloads);
+
+            var filename2 = "Morning.jpg";
+            var container = "Activity04B";
+            var fullpath2 = Path.Combine(Uploads, filename2);
+            await client.DeleteFile(filename2, container);
+            await client.UploadFile(fullpath2, container);
+            await client.DownloadFile(filename2, container, Downloads);
+        }
+    }
+    
+}
